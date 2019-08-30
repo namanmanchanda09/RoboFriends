fd7c0fa12a5d885dadff12a685a3ad82aaa05d05
namanmanchanda09 <naman.mohan09@outlook.com>
2019-08-30 22:54:59 +0530

    Updates

---
+++ precache-manifest.e7b143559688d38e6d2a009673e71f70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3870e3f4781a2f84c03d792c6f76ba09",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "56f02c62be079d43fbd2",
+    "url": "/robofriends/static/css/main.21e7c88a.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "56f02c62be079d43fbd2",
+    "url": "/robofriends/static/js/main.d35e84d4.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  },
+  {
+    "revision": "5600967edd3f1a027665ba2572f23016",
+    "url": "/robofriends/static/media/SEGA.5600967e.woff"
+  }
+]);
